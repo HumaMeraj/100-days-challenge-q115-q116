90799dc5ad2fd8bfaeb951ff7361a26bd003f837
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-19 00:33:40 +0500

    File of q116

---
+++ q116.js
@@ -0,0 +1,30 @@
+//Create a switch case that matches several cases to the same code block, representing seasons.
+// This function logs the season based on the month
+function logSeason(month) {
+    switch (month) {
+        case 12:
+        case 1:
+        case 2:
+            console.log("Winter");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("Spring");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("Summer");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log("Fall");
+            break;
+        default:
+            console.log("Invalid month");
+            break;
+    }
+}
+logSeason(3); // Outputs: Spring
